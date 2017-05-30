0f997f0c34fa05f7a21220b1f63c18d79191f525
avgtech <info@avgtech.ir>
2017-05-30 14:01:06 +0430

    Third Commit.

--- MyApplication/Program.cs
+++ MyApplication/Program.cs
@@ -11,7 +11,12 @@ namespace MyApplication
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World...");
+            Console.WriteLine("Hello World 1");
+
+            Console.WriteLine("Hello World 2");
+            
+            Console.WriteLine("Hello World 3");
+
         }
     }
 }
